26ea961d2a27bf9c0eddde8977e90ef96a62ed51
Edmond <mredmondwang@gmail.com>
2023-07-06 16:18:51 +0400

    Add nav bar and 5 initial screens

--- FrontEnd/App.js
+++ FrontEnd/App.js
@@ -4,15 +4,33 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import HomeScreen from "./screens/HomeScreen";
+import FindUserScreen from "./screens/FindUserScreen";
+import CardsScreen from './screens/CardsScreen';
+import MeScreen from './screens/MeScreen';
+import PayRequestScreen from './screens/PayRequestScreen';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-const Stack = createNativeStackNavigator();
+// const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
+      <Tab.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#e9e7e2',
+          },
+          headerTintColor: '#000000',
+          headerTintStyle:{
+            fontWeight: 'bold',
+          }
+        }}>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name='Cards' component={CardsScreen} />
+        <Tab.Screen name='Pay/Request' component={PayRequestScreen} />
+        <Tab.Screen name='Me' component={MeScreen} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
--- FrontEnd/screens/HomeScreen.js
+++ FrontEnd/screens/HomeScreen.js
@@ -1,11 +1,39 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigation } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import FindUserScreen from './FindUserScreen';
+
+const Stack = createNativeStackNavigator();
+
 
 const HomeScreen = () => {
+
+    const navigation = useNavigation();
+
+    // useLayoutEffect(() => {
+    //     navigation.setOptions({
+    //         headerShown: false,
+    //     });
+    // }, []);
+
+
+    // Fetching Past Transactions
+    // useEffect(() => {
+
+    // }, [])
+    // If [] empty, only runs once on mount
+
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
+    <Stack.Navigator>
+        <Stack.Screen name='FindUser' component={FindUserScreen} />
+    </Stack.Navigator>
+    // <Tab.Navigator>
+    //     <Tab.Screen name='Cards' component={CardsScreen} />
+    //     <Tab.Screen name='PayRequest' component={PayRequestScreen} />
+    //     <Tab.Screen name='Me' component={MeScreen} />
+    // </Tab.Navigator>
   )
 }
 
