0255ef80b9cae82a25e7110dd80b3d97077a85dd
LiuHenry9800 <henryliu9800@gmail.com>
2023-07-09 16:03:25 -0700

    user auth

--- BackEnd/api_routes/routes.mjs
+++ BackEnd/api_routes/routes.mjs
@@ -11,4 +11,39 @@ router.get("/",async(req,res) => {
     res.send(results).status(200);
 })
 
+
+
+router.post("/auth/create_user",async(req,res) => {
+    let collection = await db.collection("users");
+    let user_data = req.body;
+    collection.insertOne(
+    {
+            user_name:user_data.user_name,
+            user_phone_number:user_data.user_phone_number,
+            user_password:user_data.user_password,
+            user_QRCode: user_data.user_QRCode,
+            user_balance: user_data.user_balance,
+            user_card_info:[],
+            privacy_preference:user_data.user_balance,
+            user_contacts:[],
+            transactions:[],
+            requests:[],
+            user_creation_date:Date.now()
+    })
+    res.send(results).status(200);
+})
+router.post("/auth/user_login",async(req,res) => {
+    let collection = await db.collection("users");
+    let user_input = req.body;
+    let findUser = await collection.findOne({user_phone_number:user_input.user_phone_number},function(error,result){
+        if(!error){
+            res.send({}).status(200);
+        }
+        else{
+            console.error(error)
+            res.send({}).status(400);
+        }
+    })
+})
+
 export default router;
