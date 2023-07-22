27237e513e326fbdb9f6f3f52fb004b766301256
Edmond <mredmondwang@gmail.com>
2023-07-22 10:23:41 +0100

    Clean up

--- BackEnd/database/connect.mjs
+++ BackEnd/database/connect.mjs
@@ -2,19 +2,17 @@ import { MongoClient } from "mongodb";
 const connect_to_db = process.env.ATLAS_URI || "";
 const client = new MongoClient(connect_to_db);
 
-
 let connection;
-try{
-    connection = await client.connect();
-}
-catch(error){
-    console.error(error);
+try {
+  connection = await client.connect();
+} catch (error) {
+  console.error(error);
 }
 
 let db = connection.db();
-export function db_ref(){
-    return db;
+export function db_ref() {
+  return db;
 }
-export function client_ref(){
-    return client;
-};
+export function client_ref() {
+  return client;
+}
--- BackEnd/loadEnvironment.mjs
+++ BackEnd/loadEnvironment.mjs
@@ -1,4 +1,3 @@
-import dotenv from "dotenv"
-
+import dotenv from "dotenv";
 
 dotenv.config();
